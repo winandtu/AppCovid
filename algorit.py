213ffa74b9d26a021374e7cec726237db333586a
winandtu <55026642+winandtu@users.noreply.github.com>
2020-12-14 19:15:00 -0500

    se añadio nuevas cosas

--- algorit.py
+++ algorit.py
@@ -1,7 +1,9 @@
 print("Hola elimine un archivo y voy hacer un nuevo commit")
+
+
 n= input("ingresa tu nombre: ")
 
 if(n=="Danilo"):
     print("te llamas danilo")
 else:
-    print("Danilo Arevalo")
+    print("No te llamas Danilo")
