b8940b37f7b1588ce47384be43b4f056234befab
winandtu <55026642+winandtu@users.noreply.github.com>
2020-12-16 16:47:31 -0500

    inicio de todo

--- algorit.py
+++
@@ -1,9 +0,0 @@
-print("Hola elimine un archivo y voy hacer un nuevo commit")
-
-
-n= input("ingresa tu nombre: ")
-
-if(n=="Danilo"):
-    print("te llamas daniloytytytytyrerereweweioioio")
-else:
-    print("No te llamas Danilo tienes otro sdfdffffff")
