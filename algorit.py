d3192ea5a1def73ffa4e9d1342c2d8db2853761d
winandtu <55026642+winandtu@users.noreply.github.com>
2020-12-16 17:13:02 -0500

    dfgdfgdf

--- algorit.py
+++ algorit.py
@@ -4,7 +4,6 @@
 print("---------- BIENVENIDO NOMBRE--------------")
 print("información")
 
-<<<<<<< HEAD
 
 #nombre= input("Ingrese el nombre del paciente: ")
 #cc= int(input("Ingrese cedula del paciente: "))
@@ -16,10 +15,4 @@ print("información")
 def Datos():
     datos=[]
     
-        
-=======
-if(n=="Danilo"):
-    print("te llamas Andres")
-else:
-    print("No te llamas Danilo tienes otro sdfdffffff")
->>>>>>> 2af6cb735219366838e610b23c6f3156afb68db2
+
