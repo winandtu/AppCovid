02a7d1efd089477eb2d78d85d6416c3972c419ef
AndresGM69 <76060434+AndresGM69@users.noreply.github.com>
2020-12-16 20:34:14 -0500

    Preguntas

--- algorit.py
+++ algorit.py
@@ -1,9 +1,29 @@
-print("Hola elimine un archivo y voy hacer un nuevo commit")
+DatosRigor=[]
+DiagnosticoPrevio=[]
+IngresarPaciente="si"
 
-
-n= input("ingresa tu nombre: ")
-
-if(n=="Danilo"):
-    print("te llamas Andres")
-else:
-    print("No te llamas Danilo tienes otro sdfdffffff")
+def Datos_Paciente():
+    Nombre=input("Ingrese el nombre del paciente:\n")
+    Cedula=int(input("Ingrese el numero de cedula:\n"))
+    Sexo=input("Ingrese el sexo del paciente:\n")
+    Edad=int(input("Ingrese la edad del paciente:\n"))
+    Temperatura=int(input("Ingrese la temperatura del paciente:\n"))
+    Fila=[Nombre,Cedula,Sexo,Edad,Temperatura]
+    return Fila
+def Preguntas_Diagnostico():
+    #Probabilidad de Covid-19 alta
+    Ahogo=input("¿Siente sensacion de ahogo?")
+    Olfato_Gusto=input("¿Ha perdido el sentido del gusto y del olfato?")
+    Dolor_Pecho=("¿Siente solor o presion en el pecho?")
+    #Probabilidad de Covid-19 baja
+    MalestarGarganta=input("¿Presenta malestar en la garganta?")
+    Tos=input("¿Tiene tos seca?")
+    Casancio=input("¿Siente cansancio?")
+    Fila=[Ahogo,Olfato_Gusto,Dolor_Pecho,MalestarGarganta,Tos,Casancio]
+    return Fila
+    
+while (IngresarPaciente!="salir"):
+    IngresarPaciente=input("¿Desea ingresar un nuevo paciente si o no?\n>>")
+    if(IngresarPaciente=="si"):
+        DatosRigor.append((Datos_Paciente()))
+        DiagnosticoPrevio.append(Preguntas_Diagnostico())
