3a4899bee1e9d0f13f36762f72daf92155fb15b1
winandtu <55026642+winandtu@users.noreply.github.com>
2020-12-18 01:28:15 -0500

    Corrigiendo algunos errores de escritura e intentando imprimir los datos de forma ordenada, mañana intento de nuevo xd

--- algorit.py
+++ algorit.py
@@ -10,19 +10,24 @@ def Datos_Paciente():
     Temperatura=int(input("Ingrese la temperatura del paciente:\n"))
     Fila=[Nombre,Cedula,Sexo,Edad,Temperatura]
     return Fila
+
 def Preguntas_Diagnostico():
+    #texto=""
     #Probabilidad de Covid-19 alta
     Ahogo=input("¿Siente sensacion de ahogo?")
     Olfato_Gusto=input("¿Ha perdido el sentido del gusto y del olfato?")
-    Dolor_Pecho=("¿Siente solor o presion en el pecho?")
+    Dolor_Pecho=input("¿Siente dolor o presion en el pecho?")
+    #doloPecho=("¿Siente dolor o presion en el pecho?", Dolor_Pecho)
     #Probabilidad de Covid-19 baja
     MalestarGarganta=input("¿Presenta malestar en la garganta?")
     Tos=input("¿Tiene tos seca?")
     Casancio=input("¿Siente cansancio?")
+    #texto+= "Sensacion de ahogo: "+Ahogo+ "\n Perdida del gusto y el olfato: "+Olfato_Gusto
     Fila=[Ahogo,Olfato_Gusto,Dolor_Pecho,MalestarGarganta,Tos,Casancio]
+    #Fila= [texto]
     return Fila
     
-while (IngresarPaciente!="salir"):
+while (IngresarPaciente!="no"):
     print("BIENVENIDO A LA PRECONSULTA")
     print("POR FAVOR RESPONDA A LAS PREGUNTAS CON SI O NO EN MINUSCULAS")
     IngresarPaciente=input("¿Desea ingresar un nuevo paciente si o no?\n>>")
@@ -30,3 +35,7 @@ while (IngresarPaciente!="salir"):
         DatosRigor.append((Datos_Paciente()))
         DiagnosticoPrevio.append(Preguntas_Diagnostico())
 
+def main():
+    print(DatosRigor)
+    print(DiagnosticoPrevio)
+main()
