725f60686e8146f16e8f07563fafa04ca15cec02
winandtu <55026642+winandtu@users.noreply.github.com>
2020-12-20 20:34:10 -0500

    app completa

--- algorit.py
+++ algorit.py
@@ -1,41 +1,344 @@
-DatosRigor=[]
-DiagnosticoPrevio=[]
-IngresarPaciente="si"
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
-def Datos_Paciente():
-    Nombre=input("Ingrese el nombre del paciente:\n")
-    Cedula=int(input("Ingrese el numero de cedula:\n"))
-    Sexo=input("Ingrese el sexo del paciente:\n")
-    Edad=int(input("Ingrese la edad del paciente:\n"))
-    Temperatura=int(input("Ingrese la temperatura del paciente:\n"))
-    Fila=[Nombre,Cedula,Sexo,Edad,Temperatura]
-    return Fila
 
-def Preguntas_Diagnostico():
-    #texto=""
-    #Probabilidad de Covid-19 alta
-    Ahogo=input("¿Siente sensacion de ahogo?")
-    Olfato_Gusto=input("¿Ha perdido el sentido del gusto y del olfato?")
-    Dolor_Pecho=input("¿Siente dolor o presion en el pecho?")
-    #doloPecho=("¿Siente dolor o presion en el pecho?", Dolor_Pecho)
-    #Probabilidad de Covid-19 baja
-    MalestarGarganta=input("¿Presenta malestar en la garganta?")
-    Tos=input("¿Tiene tos seca?")
-    Casancio=input("¿Siente cansancio?")
-    #texto+= "Sensacion de ahogo: "+Ahogo+ "\n Perdida del gusto y el olfato: "+Olfato_Gusto
-    Fila=[Ahogo,Olfato_Gusto,Dolor_Pecho,MalestarGarganta,Tos,Casancio]
-    #Fila= [texto]
-    return Fila
+
+
+
+#Funcion de crear  y registrar los pacientes
+
+def AppCoron():
     
-while (IngresarPaciente!="no"):
-    print("BIENVENIDO A LA PRECONSULTA")
-    print("POR FAVOR RESPONDA A LAS PREGUNTAS CON SI O NO EN MINUSCULAS")
-    IngresarPaciente=input("¿Desea ingresar un nuevo paciente si o no?\n>>")
-    if(IngresarPaciente=="si"):
-        DatosRigor.append((Datos_Paciente()))
-        DiagnosticoPrevio.append(Preguntas_Diagnostico())
+    #Se crean las matrices que guardaran los datos 
+    MediaProbabilidad=[]
+    AltaProbabilidad=[]
+    BajaProbabilidad=[]
+    DatosRigor=[]
+    Sintomas=[]
+    IngresarPaciente="si"
 
-def main():
-    print(DatosRigor)
-    print(DiagnosticoPrevio)
-main()
+    print("....................................................")
+    print("\nBIENVENIDO A LA PRECONSULTA\n")
+    print("Ingrese los siguientes datos del paciente:\n")
+  #Funcion donde se registraran los datos generales
+    def Datos_Paciente():
+        print("\n")
+        Nombre=input("Ingrese el nombre:\n>>")
+        Cedula=int(input("Ingrese el numero de cedula:\n>>"))
+        Sexo=input("Ingrese el sexo (M/F):\n")
+        Edad=int(input("Ingrese la edad:\n"))
+        Fila=[Nombre,Cedula,Sexo,Edad]
+        return Fila
+
+    #Funcion donde se registraran los sintomas que tiene cada paciente
+    
+    def Preguntas_Diagnostico():
+        #Probabilidad de Covid-19 alta
+        print('\nPor favor ingrese los siguientes datos del paciente '+'\n'+
+            'y responda las preguntas con "si" o "no".')
+        Fiebre=input("¿Tiene fiebre?\n>>")
+        Ahogo=input("¿Siente sensacion de ahogo?\n>>")
+        Olfato_Gusto=input("¿Ha perdido el sentido del gusto y del olfato?\n>>")
+        Dolor_Pecho=input("¿Siente solor o presion en el pecho?\n>>")
+        #Probabilidad de Covid-19 baja
+        MalestarGarganta=input("¿Presenta malestar en la garganta?\n>>")
+        Tos=input("¿Tiene tos seca?\n>>")
+        Casancio=input("¿Siente cansancio?\n>>")
+        Fila=[Fiebre,Ahogo,Olfato_Gusto,Dolor_Pecho,MalestarGarganta,Tos,Casancio]
+        return Fila
+
+    #Ingreso de Datos y Creacion de los Arreglos
+    #Se guardaran los datos de cada paciente en cada arreglo
+    while(IngresarPaciente=="si"):
+        DatosRigor.append((Datos_Paciente()))
+        Sintomas.append(Preguntas_Diagnostico())
+        IngresarPaciente=input("¿Desea Ingresar un nuevo paciente?\n>>")
+
+    for i in range(0,len(Sintomas)): 
+        c=0
+        for j in range(0,7):        
+            if Sintomas[i][j]=="si":
+                c+=1   
+        if c==7:
+            AltaProbabilidad.append(DatosRigor[i])           
+        elif c<=3:
+            BajaProbabilidad.append(DatosRigor[i])        
+        elif c>3 and c<7:
+            MediaProbabilidad.append(DatosRigor[i])
+
+
+    #Estudio estadistico
+
+    #Cantidad de personas por rango de probabilidad
+
+    CantidadDePersonasAltaProbabilidad=len(AltaProbabilidad)
+    CantidadDePersonasMediaProbabilidad=len(MediaProbabilidad)
+    CantidadDePersonasBajaProbabilidad=len(BajaProbabilidad)
+
+
+    #### Estudio para alta probabilidad
+
+    #Cantidad de hombres y mujeres
+
+    CantHomAltPro=0
+    CantMujAltPro=0  
+    for i in range(0,len(AltaProbabilidad)):        
+        if AltaProbabilidad[i][2]=="M":            
+            CantHomAltPro+=1
+        if AltaProbabilidad[i][2]=="F":
+            CantMujAltPro+=1
+
+    #Rangos de edades
+    EntreCeroYQuinceAños=0
+    EntreQuinceYCuarenta=0
+    EntreCuarentaySesenta=0
+    DeSesentaEnAdelante=0
+
+    for i in range(0,len(AltaProbabilidad)):
+        if AltaProbabilidad[i][3]>=0 and AltaProbabilidad[i][3]<=15:
+            EntreCeroYQuinceAños+=1
+        if AltaProbabilidad[i][3]>15 and AltaProbabilidad[i][3]<=40:
+            EntreQuinceYCuarenta+=1
+        if AltaProbabilidad[i][3]>40 and AltaProbabilidad[i][3]<=60:
+            EntreCuarentaySesenta+=1
+        if AltaProbabilidad[i][3]>60:
+            DeSesentaEnAdelante+=1
+
+    #### Estudio para media probabilidad
+
+    #Cantidad de hombres y mujeres
+
+    CantHomMedPro=0
+    CantMujMedPro=0   
+    for i in range(0,len(MediaProbabilidad)):
+        if MediaProbabilidad[i][2]=="M":
+            CantHomMedPro+=1
+        if MediaProbabilidad[i][2]=="F":
+            CantMujMedPro+=1
+
+    #Rangos de edades
+    EntreCeroYQuinceAñosM=0
+    EntreQuinceYCuarentaM=0
+    EntreCuarentaySesentaM=0
+    DeSesentaEnAdelanteM=0
+    for i in range(0,len(MediaProbabilidad)):
+        if MediaProbabilidad[i][3]>=0 and MediaProbabilidad[i][3]<=15:
+            EntreCeroYQuinceAñosM+=1
+        if MediaProbabilidad[i][3]>15 and MediaProbabilidad[i][3]<=40:
+            EntreQuinceYCuarentaM+=1
+        if MediaProbabilidad[i][3]>40 and MediaProbabilidad[i][3]<=60:
+            EntreCuarentaySesentaM+=1
+        if MediaProbabilidad[i][3]>60:
+            DeSesentaEnAdelanteM+=1
+
+    #### Estudio para baja probabilidad
+
+    #Cantidad de hombres y mujeres
+
+    CantHomBajPro=0
+    CantMujBajPro=0
+    for i in range(0,len(BajaProbabilidad)):
+        if BajaProbabilidad[i][2]=="M":
+            CantHomBajPro+=1
+        if BajaProbabilidad[i][2]=="F":
+            CantMujBajPro+=1
+
+    #Rangos de edades
+    EntreCeroYQuinceAñosB=0
+    EntreQuinceYCuarentaB=0
+    EntreCuarentaySesentaB=0
+    DeSesentaEnAdelanteB=0
+    for i in range(0,len(BajaProbabilidad)):
+        if BajaProbabilidad[i][3]>=0 and BajaProbabilidad[i][3]<=15:
+            EntreCeroYQuinceAñosB+=1
+        if BajaProbabilidad[i][3]>15 and BajaProbabilidad[i][3]<=40:
+            EntreQuinceYCuarentaB+=1
+        if BajaProbabilidad[i][3]>40 and BajaProbabilidad[i][3]<=60:
+            EntreCuarentaySesentaB+=1
+        if BajaProbabilidad[i][3]>60:
+            DeSesentaEnAdelanteB+=1
+
+    #Se categorizan los nombres segun el Rango De Probabilidad
+    print("------------------NOMBRES DE PACIENTES  POR RANGO DE PROBABILIDAD-------------------------")
+    print("\n.........................................................................................")
+    print("Los pacientes que tiene una probabilidad alta de  tener Covid-19 son:")
+    
+    for i in range(0,len(AltaProbabilidad),1):
+        
+        print(i+1,": ",AltaProbabilidad[i][0])
+
+    print("\n..........................................................................................")
+    print("Los pacientes que tiene una probabilidad media de  tener Covid-19 son:")
+    
+
+    for i in range(0,len(MediaProbabilidad),1):
+        print(i+1,": ",MediaProbabilidad[i][0])
+    
+    print("\n............................................................................................")
+    print("Los pacientes que tiene una probabilidad baja de  tener Covid-19 son:")
+    for i in range(0,len(BajaProbabilidad),1):
+        print(i+1,": ",BajaProbabilidad[i][0])
+    print("\n............................................................................................\n")
+
+    #Se crean las graficas de cada rango
+    #Graficas por medio de matplotlib        
+    Graficas=input('¿Desea obtener graficas por cada rango de probabilidaes? - (si indica "no" podrá eligir generarlas todas) - ')
+    if(Graficas=="no"):
+        k=input("¿Desea generarlas todas?")
+    while Graficas=="si":    
+        print("- Según la cantidad de hombres y mujeres"+"\n"+"1. Alta probabilidad"+"\n"+"2. Media probabilidad"+"\n"+"3. Baja probabilidad"+"\n"+
+              "- Según el rango de edades"+"\n"+"4. Alta probabilidad"+"\n"+"5. Media probabilidad"+"\n"+"6. Baja probabilidad"+"\n"+"7. Todas las probabilidades")
+        p=int(input("Marque el número correspondiente: "))
+        if(p>=1 and p<=3):
+            if(p==1):
+                p=0
+            elif(p==3):
+                p=4
+            nme=['1-Alta probabiliad en hombres y mujeres.png','2-Media probabilidad en hombres y mujeres.png','3-Baja probabilidad en hombres y mujeres.png']           
+            aho=[CantHomAltPro,CantMujAltPro,CantHomMedPro,CantMujMedPro,CantHomBajPro,CantMujBajPro]
+            tlu=['Alta probabiliad en hombres y mujeres','Media probabilidad en hombres y mujeres','Baja probabilidad en hombres y mujeres']
+            Legendas=["Hombres","Mujeres"]
+            sizes=[aho[p],aho[p+1]]
+            fig1,ax1=plt.subplots()
+            _, _, texto=ax1.pie(sizes,labels=Legendas,autopct='%1.1f%%',startangle=90)
+            for tex in texto:
+                tex.set_color('white')
+            ax1.axis('equal')
+            if(p==2):
+                p=1
+            elif(p==4):
+                p=2
+            plt.title(tlu[p])
+            plt.savefig(nme[p])
+        elif(p==7):
+            Legendas["Alta probabilidad","Media probabilidad","Baja probabilidad"]
+            sizes=[CantidadDePersonasAltaProbabilidad,CantidadDePersonasMediaProbabilidad,CantidadDePersonasBajaProbabilidad]
+            fig1,ax1=plt.subplots()
+            _, _, texto=ax1.pie(sizes,labels=Legendas,autopct='%1.1f%%',startangle=90)
+            for tex in texto:
+                tex.set_color('white')
+            ax1.axis('equal')
+            plt.title("Proabilidades totales")
+            plt.savefig("Todas las probabilidades.png")      
+        else:
+            if(p==4):
+                p=0
+            elif(p==5):
+                p=4
+            elif(p==6):
+                p=8
+            nme2=['4-Alta probabilidad entre 0 y 60 años.png','5-Media probabilidad entre 0 y 60 años.png','6-Baja probabilidad entre 0 y 60 años.png']
+            aho2=[EntreCeroYQuinceAños,EntreQuinceYCuarenta,EntreCuarentaySesenta,DeSesentaEnAdelante,EntreCeroYQuinceAñosM,EntreQuinceYCuarentaM,EntreCuarentaySesentaM,
+                  DeSesentaEnAdelanteM,EntreCeroYQuinceAñosB,EntreQuinceYCuarentaB,EntreCuarentaySesentaB,DeSesentaEnAdelanteB]
+            tlu2=['Alta probabilidad entre 0 y 60 años','Media probabilidad entre 0 y 60 años','Baja probabilidad entre 0 y 60 años']
+            Legendas=["De 0 a 15",">15, <=40",">40, <=60","Más de 60"]
+            sizes=[aho2[p],aho2[p+1],aho2[p+2],aho2[p+3]]
+            fig1,ax1=plt.subplots()
+            _, _, texto=ax1.pie(sizes,labels=Legendas,autopct='%1.1f%%',startangle=90)
+            for tex in texto:
+                tex.set_color('white')
+            ax1.axis('equal')
+            if(p==4):
+                p=1
+            elif(p==8):
+                p=2
+            plt.title(tlu2[p])
+            plt.savefig(nme2[p])
+        print("")
+        #El usuario tiene la opcion de escoger las distintas graficas o si desea descargar todas
+        Graficas=input("¿Desea obtener otra grafica? - ")
+        print("")
+    if(k=="si"):
+            for p in range(1,8):   
+                if(p>=1 and p<=3):
+                    if(p==1):
+                        p=0
+                    elif(p==3):
+                        p=4
+                    nme=['1-Alta probabiliad en hombres y mujeres.png','2-Media probabilidad en hombres y mujeres.png','3-Baja probabilidad en hombres y mujeres.png']           
+                    aho=[CantHomAltPro,CantMujAltPro,CantHomMedPro,CantMujMedPro,CantHomBajPro,CantMujBajPro]
+                    tlu=['Alta probabiliad en hombres y mujeres','Media probabilidad en hombres y mujeres','Baja probabilidad en hombres y mujeres']
+                    Legendas=["Hombres","Mujeres"]
+                    sizes=[aho[p],aho[p+1]]
+                    fig1,ax1=plt.subplots()
+                    _, _, texto=ax1.pie(sizes,labels=Legendas,autopct='%1.1f%%',startangle=90)
+                    for tex in texto:
+                        tex.set_color('white')
+                    ax1.axis('equal')
+                    if(p==2):
+                        p=1
+                    elif(p==4):
+                        p=2
+                    plt.title(tlu[p])
+                    plt.savefig(nme[p])
+                elif(p==7):
+                    Legendas=["Alta probabilidad","Media probabilidad","Baja probabilidad"]
+                    sizes=[CantidadDePersonasAltaProbabilidad,CantidadDePersonasMediaProbabilidad,CantidadDePersonasBajaProbabilidad]
+                    fig1,ax1=plt.subplots()
+                    _, _, texto=ax1.pie(sizes,labels=Legendas,autopct='%1.1f%%',startangle=90)
+                    for tex in texto:
+                        tex.set_color('white')
+                    ax1.axis('equal')
+                    plt.title("Proabilidades totales")
+                    plt.savefig("Todas las probabilidades.png")  
+                else:
+                    if(p==4):
+                        p=0
+                    elif(p==5):
+                        p=4
+                    elif(p==6):
+                        p=8
+                    nme2=['4-Alta probabilidad entre 0 y 60 años.png','5-Media probabilidad entre 0 y 60 años.png','6-Baja probabilidad entre 0 y 60 años.png']
+                    aho2=[EntreCeroYQuinceAños,EntreQuinceYCuarenta,EntreCuarentaySesenta,DeSesentaEnAdelante,EntreCeroYQuinceAñosM,EntreQuinceYCuarentaM,EntreCuarentaySesentaM,
+                          DeSesentaEnAdelanteM,EntreCeroYQuinceAñosB,EntreQuinceYCuarentaB,EntreCuarentaySesentaB,DeSesentaEnAdelanteB]
+                    tlu2=['Alta probabilidad entre 0 y 60 años','Media probabilidad entre 0 y 60 años','Baja probabilidad entre 0 y 60 años']
+                    Legendas=["De 0 a 15",">15, <=40",">40, <=60","Más de 60"]
+                    sizes=[aho2[p],aho2[p+1],aho2[p+2],aho2[p+3]]
+                    fig1,ax1=plt.subplots()
+                    _, _, texto=ax1.pie(sizes,labels=Legendas,autopct='%1.1f%%',startangle=90)
+                    for tex in texto:
+                        tex.set_color('white')
+                    ax1.axis('equal')
+                    if(p==4):
+                        p=1
+                    elif(p==8):
+                        p=2
+                    plt.title(tlu2[p])
+                    plt.savefig(nme2[p])
+
+    print("\nGRAFICAS DESCARGADAS |//////////////////////|\n")
+    print("           GRACIAS POR SU CONSULTA")
+
+#Creación del menu  
+def menu():
+   
+    salir=3
+    
+    while True:
+        archivo= open("Titulo.txt")
+        inf= open("info.txt")
+        print(archivo.read())
+
+        print("\n1. Iniciar consulta", "\n2. Informacion del programa ", "\n3. Salir")
+        op= int(input("Por favor, ingresa tu opción: "))
+        #regresa= int(input("¿Desea regresar al menu prinpal?\n"+ "\n1.Si"+ "\n2.No"+"\n>>"))
+
+        if(op==1):
+            AppCoron()
+            break
+        elif(op==2):
+            print(inf.read())
+            regresa= int(input("¿Desea regresar al menu prinpal?\n"+ "\n1.Si"+ "\n2.No"+"\n>>"))
+            if(regresa==1):
+                menu()
+            else:
+                print("\nGRACIAS POR UTILIZAR LA APLICACIÓN")
+                break
+            break
+        elif(op==salir):
+            print("\nGRACIAS POR UTILIZAR LA APLICACIÓN")
+            break 
+
+#Se retorna todo el programa  
+menu()
