385f98c7dcc5244761d4388176981d45e8d2b8f1
winandtu <55026642+winandtu@users.noreply.github.com>
2020-12-20 20:14:21 -0500

    fsdfdsfs

--- appCovid.py
+++ appCovid.py
@@ -4,14 +4,13 @@ import matplotlib.pyplot as plt
 
 
 
-#Matrices donde se guardaran los datos
+
 
 #Funcion de crear  y registrar los pacientes
 
-
-
 def AppCoron():
     
+    #Se crean las matrices que guardaran los datos 
     MediaProbabilidad=[]
     AltaProbabilidad=[]
     BajaProbabilidad=[]
@@ -22,20 +21,22 @@ def AppCoron():
     print("....................................................")
     print("\nBIENVENIDO A LA PRECONSULTA\n")
   
+  #Funcion donde se registraran los datos generales
     def Datos_Paciente():
         print("Ingrese los siguientes datos del paciente:\n")
         Nombre=input("Ingrese el nombre:\n>>")
         Cedula=int(input("Ingrese el numero de cedula:\n>>"))
         Sexo=input("Ingrese el sexo (M/F):\n")
         Edad=int(input("Ingrese la edad:\n"))
-        Temperatura=int(input("Ingrese la temperatura:\n>>"))
-        Fila=[Nombre,Cedula,Sexo,Edad,Temperatura]
+        Fila=[Nombre,Cedula,Sexo,Edad]
         return Fila
 
+    #Funcion donde se registraran los sintomas que tiene cada paciente
     def Preguntas_Diagnostico():
         #Probabilidad de Covid-19 alta
         print('\nPor favor ingrese los siguientes datos del paciente '+'\n'+
             'y responda las preguntas con "si" o "no".')
+        Fiebre=input("¿Tiene fiebre?\n>>")
         Ahogo=input("¿Siente sensacion de ahogo?\n>>")
         Olfato_Gusto=input("¿Ha perdido el sentido del gusto y del olfato?\n>>")
         Dolor_Pecho=input("¿Siente solor o presion en el pecho?\n>>")
@@ -43,11 +44,11 @@ def AppCoron():
         MalestarGarganta=input("¿Presenta malestar en la garganta?\n>>")
         Tos=input("¿Tiene tos seca?\n>>")
         Casancio=input("¿Siente cansancio?\n>>")
-        Fila=[Ahogo,Olfato_Gusto,Dolor_Pecho,MalestarGarganta,Tos,Casancio]
+        Fila=[Fiebre,Ahogo,Olfato_Gusto,Dolor_Pecho,MalestarGarganta,Tos,Casancio]
         return Fila
 
     #Ingreso de Datos y Creacion de los Arreglos
-
+    #Se guardaran los datos de cada paciente en cada arreglo
     while(IngresarPaciente=="si"):
         DatosRigor.append((Datos_Paciente()))
         Sintomas.append(Preguntas_Diagnostico())
@@ -55,14 +56,14 @@ def AppCoron():
 
     for i in range(0,len(Sintomas)): 
         c=0
-        for j in range(0,6):        
+        for j in range(0,7):        
             if Sintomas[i][j]=="si":
                 c+=1   
-        if c==6:
+        if c==7:
             AltaProbabilidad.append(DatosRigor[i])           
         elif c<=3:
             BajaProbabilidad.append(DatosRigor[i])        
-        elif c>3 and c<6:
+        elif c>3 and c<7:
             MediaProbabilidad.append(DatosRigor[i])
 
 
@@ -70,9 +71,9 @@ def AppCoron():
 
     #Cantidad de personas por rango de probabilidad
 
-    #CantidadDePersonasAltaProbabilidad=len(AltaProbabilidad)
-    #CantidadDePersonasMediaProbabilidad=len(MediaProbabilidad)
-    #CantidadDePersonasBajaProbabilidad=len(BajaProbabilidad)
+    CantidadDePersonasAltaProbabilidad=len(AltaProbabilidad)
+    CantidadDePersonasMediaProbabilidad=len(MediaProbabilidad)
+    CantidadDePersonasBajaProbabilidad=len(BajaProbabilidad)
 
 
     #### Estudio para alta probabilidad
@@ -92,6 +93,7 @@ def AppCoron():
     EntreQuinceYCuarenta=0
     EntreCuarentaySesenta=0
     DeSesentaEnAdelante=0
+
     for i in range(0,len(AltaProbabilidad)):
         if AltaProbabilidad[i][3]>=0 and AltaProbabilidad[i][3]<=15:
             EntreCeroYQuinceAños+=1
@@ -156,26 +158,36 @@ def AppCoron():
         if BajaProbabilidad[i][3]>60:
             DeSesentaEnAdelanteB+=1
 
-    #Nombres segun Rango De Probabilidad
-
+    #Se categorizan los nombres segun el Rango De Probabilidad
+    print("------------------NOMBRES DE PACIENTES  POR RANGO DE PROBABILIDAD-------------------------")
+    print("\n.........................................................................................")
     print("Los pacientes que tiene una probabilidad alta de  tener Covid-19 son:")
+    
     for i in range(0,len(AltaProbabilidad),1):
-        print(AltaProbabilidad[i][0])
+        
+        print(i+1,": ",AltaProbabilidad[i][0])
 
+    print("\n..........................................................................................")
     print("Los pacientes que tiene una probabilidad media de  tener Covid-19 son:")
-    for i in range(0,len(MediaProbabilidad),1):
-        print(MediaProbabilidad[i][0])
+    
 
+    for i in range(0,len(MediaProbabilidad),1):
+        print(i+1,": ",MediaProbabilidad[i][0])
+    
+    print("\n............................................................................................")
     print("Los pacientes que tiene una probabilidad baja de  tener Covid-19 son:")
     for i in range(0,len(BajaProbabilidad),1):
-        print(BajaProbabilidad[i][0])
+        print(i+1,": ",BajaProbabilidad[i][0])
+    print("\n............................................................................................\n")
 
+    #Se crean las graficas de cada rango
     #Graficas por medio de matplotlib        
     Graficas=input('¿Desea obtener graficas por cada rango de probabilidaes? - (si indica "no" podrá eligir generarlas todas) - ')
-    k=input("¿Desea generarlas todas?")
+    if(Graficas=="no"):
+        k=input("¿Desea generarlas todas?")
     while Graficas=="si":    
         print("- Según la cantidad de hombres y mujeres"+"\n"+"1. Alta probabilidad"+"\n"+"2. Media probabilidad"+"\n"+"3. Baja probabilidad"+"\n"+
-              "- Según el rango de edades"+"\n"+"4. Alta probabilidad"+"\n"+"5. Media probabilidad"+"\n"+"6. Baja probabilidad")
+              "- Según el rango de edades"+"\n"+"4. Alta probabilidad"+"\n"+"5. Media probabilidad"+"\n"+"6. Baja probabilidad"+"\n"+"7. Todas las probabilidades")
         p=int(input("Marque el número correspondiente: "))
         if(p>=1 and p<=3):
             if(p==1):
@@ -197,7 +209,17 @@ def AppCoron():
             elif(p==4):
                 p=2
             plt.title(tlu[p])
-            plt.savefig(nme[p])        
+            plt.savefig(nme[p])
+        elif(p==7):
+            Legendas["Alta probabilidad","Media probabilidad","Baja probabilidad"]
+            sizes=[CantidadDePersonasAltaProbabilidad,CantidadDePersonasMediaProbabilidad,CantidadDePersonasBajaProbabilidad]
+            fig1,ax1=plt.subplots()
+            _, _, texto=ax1.pie(sizes,labels=Legendas,autopct='%1.1f%%',startangle=90)
+            for tex in texto:
+                tex.set_color('white')
+            ax1.axis('equal')
+            plt.title("Proabilidades totales")
+            plt.savefig("Todas las probabilidades.png")      
         else:
             if(p==4):
                 p=0
@@ -209,7 +231,7 @@ def AppCoron():
             aho2=[EntreCeroYQuinceAños,EntreQuinceYCuarenta,EntreCuarentaySesenta,DeSesentaEnAdelante,EntreCeroYQuinceAñosM,EntreQuinceYCuarentaM,EntreCuarentaySesentaM,
                   DeSesentaEnAdelanteM,EntreCeroYQuinceAñosB,EntreQuinceYCuarentaB,EntreCuarentaySesentaB,DeSesentaEnAdelanteB]
             tlu2=['Alta probabilidad entre 0 y 60 años','Media probabilidad entre 0 y 60 años','Baja probabilidad entre 0 y 60 años']
-            Legendas=["De 0 a 15","Más de 15 y menor o igual a 40","Más de 40 y menor o igual 60","Más de 60"]
+            Legendas=["De 0 a 15",">15, <=40",">40, <=60","Más de 60"]
             sizes=[aho2[p],aho2[p+1],aho2[p+2],aho2[p+3]]
             fig1,ax1=plt.subplots()
             _, _, texto=ax1.pie(sizes,labels=Legendas,autopct='%1.1f%%',startangle=90)
@@ -223,10 +245,11 @@ def AppCoron():
             plt.title(tlu2[p])
             plt.savefig(nme2[p])
         print("")
+        #El usuario tiene la opcion de escoger las distintas graficas o si desea descargar todas
         Graficas=input("¿Desea obtener otra grafica? - ")
         print("")
     if(k=="si"):
-            for p in range(1,7):   
+            for p in range(1,8):   
                 if(p>=1 and p<=3):
                     if(p==1):
                         p=0
@@ -247,7 +270,17 @@ def AppCoron():
                     elif(p==4):
                         p=2
                     plt.title(tlu[p])
-                    plt.savefig(nme[p])        
+                    plt.savefig(nme[p])
+                elif(p==7):
+                    Legendas=["Alta probabilidad","Media probabilidad","Baja probabilidad"]
+                    sizes=[CantidadDePersonasAltaProbabilidad,CantidadDePersonasMediaProbabilidad,CantidadDePersonasBajaProbabilidad]
+                    fig1,ax1=plt.subplots()
+                    _, _, texto=ax1.pie(sizes,labels=Legendas,autopct='%1.1f%%',startangle=90)
+                    for tex in texto:
+                        tex.set_color('white')
+                    ax1.axis('equal')
+                    plt.title("Proabilidades totales")
+                    plt.savefig("Todas las probabilidades.png")  
                 else:
                     if(p==4):
                         p=0
@@ -259,7 +292,7 @@ def AppCoron():
                     aho2=[EntreCeroYQuinceAños,EntreQuinceYCuarenta,EntreCuarentaySesenta,DeSesentaEnAdelante,EntreCeroYQuinceAñosM,EntreQuinceYCuarentaM,EntreCuarentaySesentaM,
                           DeSesentaEnAdelanteM,EntreCeroYQuinceAñosB,EntreQuinceYCuarentaB,EntreCuarentaySesentaB,DeSesentaEnAdelanteB]
                     tlu2=['Alta probabilidad entre 0 y 60 años','Media probabilidad entre 0 y 60 años','Baja probabilidad entre 0 y 60 años']
-                    Legendas=["De 0 a 15","15< y <=40","40< y <=60","Más de 60"]
+                    Legendas=["De 0 a 15",">15, <=40",">40, <=60","Más de 60"]
                     sizes=[aho2[p],aho2[p+1],aho2[p+2],aho2[p+3]]
                     fig1,ax1=plt.subplots()
                     _, _, texto=ax1.pie(sizes,labels=Legendas,autopct='%1.1f%%',startangle=90)
@@ -273,6 +306,8 @@ def AppCoron():
                     plt.title(tlu2[p])
                     plt.savefig(nme2[p])
 
+    print("\nGRAFICAS DESCARGADAS |//////////////////////|\n")
+    print("           GRACIAS POR SU CONSULTA")
 
 #Creación del menu  
 def menu():
@@ -303,5 +338,6 @@ def menu():
         elif(op==salir):
             print("\nGRACIAS POR UTILIZAR LA APLICACIÓN")
             break 
-    
+
+#Se retorna todo el programa  
 menu()
